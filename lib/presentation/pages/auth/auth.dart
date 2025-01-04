73b019581b0c882fd09345fa7fd356509f34bd58
Agus Prayogi <agus21apy@gmail.com>
2025-01-04 20:47:36 +0700

    [ADD] forgot password with controller

--- lib/presentation/pages/auth/auth.dart
+++ lib/presentation/pages/auth/auth.dart
@@ -1,5 +1,5 @@
 export 'change_password/change_password_page.dart';
-export 'forgot_password_page.dart';
+export 'forgot_password/forgot_password_page.dart';
 export 'login/login_page.dart';
 export 'otp/otp_page.dart';
 export 'otp/widget/resend_otp_timer.dart';
